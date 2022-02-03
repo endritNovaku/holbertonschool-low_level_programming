f470c9f14f3fdc85c1450f99a2532690fe8a4682
endritNovaku <ennald@outlook.com>
2022-02-03 08:36:28 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
-int main()
+/*pritns Programming is like building a multilingual puzzle*/
+int main(void)
 {
-char str[] = "Programming is like building a multilingual puzzle\n";
+char str[] = "\"Programming is like building a multilingual puzzle\n";
 puts(str);
-return 0;
+return (0);
 }
