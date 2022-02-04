5556d22c0f4cb31039ea60eca326cb3d2a0d0c14
endritNovaku <ennald@outlook.com>
2022-02-04 03:47:17 -0800

    print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the alhabet
+ * Return: allways zero
+ */
+
+int main(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+
+putchar(alphabet);
+return (0);
+}
