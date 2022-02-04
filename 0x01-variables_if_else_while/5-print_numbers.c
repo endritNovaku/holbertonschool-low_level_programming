0bc99040c74727211959c147e7136b87826b79a2
endritNovaku <ennald@outlook.com>
2022-02-04 05:35:20 -0800

    print all sigle digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all single numbers
+ * Return: Allways 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+printf("%i", i);
+}
+return (0);
+}
