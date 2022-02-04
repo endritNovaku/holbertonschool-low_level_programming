25450824fd384ce3a8edaadb970a7ba5281fedc3
endritNovaku <ennald@outlook.com>
2022-02-04 12:47:38 -0800

    convert decimals to hexadecimals

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
+/**
+ * main - conver decimals to hexdecimals
+ * Return: Always 0
+ */
 
+int main(void)
+{
+int i;
+int j;
+char letters[] = "abcdef";
 
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+
+for (j = 10; j <= 15; j++)
+{
+putchar(letters[j - 10]);
+}
+putchar('\n');
+
+return (0);
+}
