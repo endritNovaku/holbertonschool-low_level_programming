c0862460c0bcb7a87de954e567d3247d73d6dfbf
endritNovaku <ennald@outlook.com>
2022-02-08 05:30:01 -0800

    check if is lower or uppercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,17 @@
 #include "main.h"
+/**
+ * _islower - check if a letter is lower or upper
+ * Return: Allways 0
+ */
 
 int _islower(int c)
 {
-	if ( c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
