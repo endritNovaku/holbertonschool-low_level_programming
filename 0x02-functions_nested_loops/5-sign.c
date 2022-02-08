e9af594c1ea7683a7573c3223519ed0d459464e4
endritNovaku <ennald@outlook.com>
2022-02-08 05:51:45 -0800

    check if a number is greater or lower then 0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - check if is higher or lower then 0
+ * @n: place number
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+}
