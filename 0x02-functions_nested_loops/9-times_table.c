9b0ee89a349600dabb9ad5ac67dbfabab8df9f6e
endritNovaku <ennald@outlook.com>
2022-02-08 12:37:02 -0800

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - times table
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if ((i * j) <= 9 && j != 9)
+			{
+				_putchar(i * j + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if ((i * j) > 9 && j != 9)
+			{
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ((i * j) <= 9)
+			{
+				_putchar((i * j) + '0');
+				_putchar('$');
+			}
+			else
+			{
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+				_putchar('$');
+			}
+		}
+	}
+}
