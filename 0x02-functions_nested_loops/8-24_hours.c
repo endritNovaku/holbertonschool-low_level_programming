a34b95fe0e172aa340d80c76d43056e5f32a32ff
endritNovaku <ennald@outlook.com>
2022-02-08 11:20:33 -0800

    print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,27 +5,18 @@
 
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j
 
-	for (i = 0; i <= 2; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 60; j++)
 		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (l = 0; l <= 9; l++)
-				{
-					if (i <= 2 && j <= 3)
-					{
-						_putchar(i + '0');
-						_putchar(j + '0');
-						_putchar(':');
-						_putchar(k + '0');
-						_putchar(l + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar(i / 10  + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
