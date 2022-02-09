ff07f19a6a0602b6423df2b4d115b5e7979ec407
endritNovaku <ennald@outlook.com>
2022-02-09 00:24:22 -0800

    return the sum of 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - return the the sum of 2 numbers
+ * Return: a + b
+ * @a: place a number
+ * @b: place another number
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
