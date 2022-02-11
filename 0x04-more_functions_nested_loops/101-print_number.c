d57a3fcede58afee87e312224e6fe85d8de84a22
endritNovaku <ennald@outlook.com>
2022-02-11 05:27:05 -0800

    more functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,28 +6,17 @@
 
 void print_number(int n)
 {
-int rev = 0, remainder;
-
-if (n == 0)
-{
-	_putchar('0');
-}
+unsigned int num = n;
 
 if (n < 0)
 {
 	_putchar('-');
-	n = -n;
-}
-while (n != 0)
-{
-	remainder = n % 10;
-	rev = rev * 10 + remainder;
-	n /= 10;
-	}
-while (rev != 0)
-{
-	_putchar(rev % 10 + '0');
-	rev = rev / 10;
+	num = -num;
 }
 
+if (num / 10)
+{
+	print_number(num / 10);
+}
+	_putchar((num % 10) + '0');
 }
