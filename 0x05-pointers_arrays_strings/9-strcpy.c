2408e9468e381e6e24edb0d1c039ecba09648c0e
endritNovaku <ennald@outlook.com>
2022-02-14 07:34:15 -0800

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,11 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	
-	for (i = 0; i != src['\0']; i++)
+
+	for (i = 0; i != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i++] = '/0';
+	dest[i] = '/0';
 	return (dest);
 }
