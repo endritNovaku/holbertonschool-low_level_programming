6929ab808be301be798c9500e7249f31d40a3d53
endritNovaku <ennald@outlook.com>
2022-02-14 07:09:27 -0800

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_array - print array
+ * @a: int
+ * @n: int
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+	while (i < n)
+	{
+		printf("%i", a[i]);
+		i++;
+	}
+}
