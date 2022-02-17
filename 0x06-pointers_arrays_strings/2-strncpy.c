ca1d507304a59321943e651146159bef3ff85ec9
endritNovaku <ennald@outlook.com>
2022-02-17 01:32:04 -0800

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (dest[i] != '\0' && i < n)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
