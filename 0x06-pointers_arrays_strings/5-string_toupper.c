52c8c6bd21089bb697026912ca1a8b563d2e33d7
endritNovaku <ennald@outlook.com>
2022-02-17 02:57:33 -0800

    turn to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - turn string to uppercase
+ * @s: string
+ * Return: s
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+
+	return (s);
+}
