8ea03331f3c9727ea3a32b58b80bfa3e31946b15
endritNovaku <ennald@outlook.com>
2022-02-18 00:52:14 -0800

    convert a string to rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - convert string to rot13
+ * @s: - string
+ * Return: string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char n[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (s[i] == l[j])
+			{
+				s[i] = n[j];
+			}
+		}
+	}
+
+	return (s);
+}
