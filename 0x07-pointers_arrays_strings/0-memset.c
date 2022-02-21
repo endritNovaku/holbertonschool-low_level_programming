6fc98ae72ba81b79d05d7ff2e8fdfd5d061bc4ba
endritNovaku <ennald@outlook.com>
2022-02-21 04:10:33 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i = 0;
-  
-  while (i < n)
-    {
-      s[i] = b;
-      i++;
-    }
+	unsigned int i = 0;
 
-  return (s);
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
 }
