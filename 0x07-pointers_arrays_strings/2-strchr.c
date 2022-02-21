d7b631c10dba2b3274e6397af973df5687c6a133
endritNovaku <ennald@outlook.com>
2022-02-21 05:29:12 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - find the character
+ * @s: string
+ * @c: character
+ * Return: c
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (;; s++)
+	{
+		if (*s == c) return (s);
+		if (!*s) return (NULL);
+	}
+	
+	return (NULL);
+}
