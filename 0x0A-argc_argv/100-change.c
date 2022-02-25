80917c3222530f7e97ef2a6be5f5a89ef535169b
endritNovaku <ennald@outlook.com>
2022-02-25 05:09:45 -0800

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - min number of coin change
+ * @argc: argv length
+ * @argv: array
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int coin = 0;
+	int sum = 0;
+	int arr[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 0; arr[i] != '\0'; i++)
+	{
+		while (sum + arr[i] <= atoi(argv[1]))
+		{
+			sum += arr[i];
+			coin++;
+		}
+		if (sum == atoi(argv[1]))
+		{
+			break;
+		}
+	}
+	printf("%i\n", coin);
+	return (1);
+}
