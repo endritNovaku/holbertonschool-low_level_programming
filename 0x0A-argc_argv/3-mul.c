c0be571f695ae51b9bc4dc2a9681c2934c203ad2
endritNovaku <ennald@outlook.com>
2022-02-25 03:24:34 -0800

    fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,11 @@ int main(int argc, char *argv[])
 	if (argc < 2)
 	{
 		printf("Error\n");
-		return (1);
 	}
+		return (1);
 	else
 	{
 		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
 	}
 
 	return (0);
