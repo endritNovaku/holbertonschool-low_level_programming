81b6ac84b0843bc77d97bae12fa9b427f3ac571e
endritNovaku <ennald@outlook.com>
2022-02-27 01:52:56 -0800

    libmy.a

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * _isalpha - check if is alphabet char
+ *   * @c: place a character
+ *    * Return: 0
+ *     */
+
+int _isalpha(int c)
+{
+		if (c >= 'A' && c <= 'z')
+				{
+							return (1);
+								}
+			else
+					{
+								return (0);
+									}
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * _abs - return absolute value of an int
+ *   * @n: add an integer
+ *    * Return: 0
+ *     */
+
+int _abs(int n)
+{
+		if (n > 0)
+				{
+							return (n);
+								}
+			else
+					{
+							return (n * (-1));
+								}
+}
