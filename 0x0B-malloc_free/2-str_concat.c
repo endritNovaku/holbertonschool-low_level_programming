c6999f846f477d64555aa6b9faaf6dcc5123ae4a
endritNovaku <ennald@outlook.com>
2022-03-01 05:36:26 -0800

    0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,14 @@ char *str_concat(char *s1, char *s2)
 	int i,j,k = 0;
 	char *cc;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if(s2 == NULL)
+	{
+		s2 = "";
+	}
 	for ( i = 0; s1[i] != '\0'; i++)
 	{
 		k++;
