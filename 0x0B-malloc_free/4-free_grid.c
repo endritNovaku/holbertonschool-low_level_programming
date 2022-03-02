6a673fe4c6ecdd33687efac3773bc85ab281cfb3
endritNovaku <ennald@outlook.com>
2022-03-02 01:04:43 -0800

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free grid
+ * @grid: the grid
+ * @height: height of the table
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < heigh; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
