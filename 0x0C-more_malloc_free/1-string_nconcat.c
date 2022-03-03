40021d03535e705cf248fb72e13af13a53a960ce
endritNovaku <ennald@outlook.com>
2022-03-03 04:39:20 -0800

    0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,33 +14,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *cc;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		k++;
-	}
 
 	cc = malloc(sizeof(char) * (k + 1));
 
 	if (cc == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		cc[i] = s1[i];
-	}
-	for (i = 0; s2[i] != '\0' && i < n; i++)
-	{
+	for (i = 0; s2[i] != '\0' || i < n; i++)
 		cc[i + k] = s2[i];
-	}
 
 	return (cc);
 
