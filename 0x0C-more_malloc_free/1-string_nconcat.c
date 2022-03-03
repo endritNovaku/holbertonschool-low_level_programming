f7280408008241091f54e8ad99f33954caac4528
endritNovaku <ennald@outlook.com>
2022-03-03 04:53:43 -0800

    0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i] != '\0'; i++, j++)
 		cc[j] = s1[i];
-	for (i = 0; s2[i] != '\0' || i < n; i++, j++)
+	for (i = 0; s2[i] != '\0' && i < n; i++, j++)
 		cc[j] = s2[i];
 
 	cc[j] = '\0';
