fd84263b58c8d60f563d58109383f2ace52036d3
endritNovaku <ennald@outlook.com>
2022-03-09 06:50:43 -0800

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - calculator
+ * @argv: argv
+ * @argc: argc
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+
+	}
+
+	if (get_op_func(argv[2]) == NULL && *argv[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+
+	printf("%i\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - return the sum of 2 numbers
+ * @a: number one
+ * @b: number two
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - return the sub of two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - return mul of two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - return modulo of two numbers
+ * @a: number one
+ * @b: number two
+ * Return a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
