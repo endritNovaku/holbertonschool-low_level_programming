4be251e1cdeccb56461f71f9a60af3e7144351b8
endritNovaku <ennald@outlook.com>
2022-03-09 03:15:44 -0800

    ifndef

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _FUNCTION_POINTERS_H
-#define _FUCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_H
+#define FUCTION_POINTERS_H
 #include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
