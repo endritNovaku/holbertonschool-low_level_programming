e4561e0d58309a23d9c8ce39b63536b2b5eebc49
endritNovaku <ennald@outlook.com>
2022-03-10 13:27:48 -0800

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include "variadic_functions.h"
+
+/**
+ * print_char - character
+ * @char_list: list
+ */
+void print_char(va_list char_list)
+{
+	printf("%c", va_arg(char_list, int));
+}
+
+/** 
+ * print_int - int
+ * @int_list: int list
+ */
+void print_int(va_list int_list)
+{
+	printf("%i", va_arg(int_list, int));
+}
+
+/** 
+ * print_float - float
+ * @foat_list: float list
+ */
+void print_float(va_list float_list)
+{
+	printf("%f", va_arg(float_list, double));
+}
+
+/**
+ * print_s - print string
+ * @s_list: string list
+ */
+void print_s(va_list s_list)
+{
+	printf("%s", va_arg(s_list, char *));
+}
+
+/**
+ * print_all - printf
+ * @format: format
+ */
+void print_all(const char* const format, ...)
+{
+	va_list args_list;
+	int i = 0, j;
+	char *separator = "";
+
+	print_data print_funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_s}};
+
+	va_start(args_list, format);
+
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (j < 4)
+		{
+			printf("%s", separator);
+			print_funcs[j].print(args_list);
+		}
+		separator = ", ";
+		i++;
+	}
+	printf("\n");
+	va_end(args_list);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,15 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+
+typedef struct print_data
+{
+	char *type; 
+	void (*print)(va_list arg_list);
+} print_data;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *sepearator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format , ...);
 #endif
