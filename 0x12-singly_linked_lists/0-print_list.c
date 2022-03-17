f1e6d89fde39e35f5ccd59f233b7f88cfae86564
endritNovaku <ennald@outlook.com>
2022-03-17 05:22:19 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_list - print list
+ * @h: list
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if(h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%i] %s", h->len, h->str);
+			count++;
+		}
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
