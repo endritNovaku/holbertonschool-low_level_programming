72f2ed9cbfcb2c1fbb6223e5d6b670d8f610d59b
endritNovaku <ennald@outlook.com>
2022-03-22 05:23:35 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,23 @@
 #include "lists.h"
 #include <stdio.h>
 
-/*
+/**
  * print_listint - print list
  * @h: list elements
  * Return: h
  */
 size_t print_listint(const listint_t *h)
 {
+	size_t count = 0;
+
 	while (h != NULL)
 	{
-		printf("%i\n", h->n);
+		if (!h->n)
+			printf("[0] (lil)\n");
+		else
+			printf("%i\n",h->n);
+		count++;
 		h = h->next;
 	}
-	return (0);
+	return (count);
 }
-
