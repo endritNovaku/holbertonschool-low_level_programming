fd44f09b5ac250a933f08b80c078012343db2b06
endritNovaku <ennald@outlook.com>
2022-03-22 06:55:59 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint2 - free list
+ * @head: the beginning of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (!head)
+		return;
+
+	while(*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		(*head) = tmp;
+	}
+	*head = NULL;
+}
