2aaa68fdcc2deb64dca2ac911c3ede66c64c8873
endritNovaku <ennald@outlook.com>
2022-03-24 05:41:35 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - get the bit at a given index
+ * @n: number
+ * @index: index
+ * Return: the bit at a given index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int num = (n >> index) & 1;
+
+	if (n > 0 || index > 0)
+		return (num);
+
+	return (-1);
+}
