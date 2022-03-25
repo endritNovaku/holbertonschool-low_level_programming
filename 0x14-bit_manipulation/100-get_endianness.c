da78202f960fa6f994e7df080062a6c869284df6
endritNovaku <ennald@outlook.com>
2022-03-25 03:07:37 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - endianness
+ * Return: int
+ */
+
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *c = (char *) &num;
+
+	if (*c)
+		return (1);
+	else 
+		return (0);
+}
