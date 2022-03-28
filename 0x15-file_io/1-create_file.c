8917842dbb2ca58329aeedb4765db88d1d92f159
endritNovaku <ennald@outlook.com>
2022-03-28 05:42:05 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of the file
+ * @text_content: content in the created file
+ * Return: 1 on success or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i = 0;
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+		return(-1);
+
+	if (fd == -1)
+		return (-1);
+
+	write(fd, text_content, i);
+
+	close(fd);
+	return (1);
+}
