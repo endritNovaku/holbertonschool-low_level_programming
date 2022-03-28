d959c3b2a576d663faab1549c67edc2cdc9c87ac
endritNovaku <ennald@outlook.com>
2022-03-28 04:29:09 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a txt file
+ * @filename: file to be readed
+ * @letters: max number of letters
+ * Return: length of the file
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f; 
+	int rd, wr;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	rd = read(f, buff, letters);
+	if (rd == -1)
+		return (0);
+
+	wr = write(STDOUT_FILENO, buff, rd);
+	if (wr == -1)
+		return (0);
+
+	free(buff);
+	close(f);
+	return (rd);
+}
