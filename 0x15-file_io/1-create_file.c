383e7e03acf108236a6d4e628367c51373be598d
endritNovaku <ennald@outlook.com>
2022-03-28 05:50:19 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 
 /**
  * create_file - create a file
- * @filename: name of the file
+ C* @filename: name of the file
  * @text_content: content in the created file
  * Return: 1 on success or -1
  */
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | 0_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
