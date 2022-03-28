82b0230fc20a9202df660ec1d6ac9cb251714baa
endritNovaku <ennald@outlook.com>
2022-03-28 05:46:41 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,11 +9,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, w;
 	int i = 0;
 
-	if (text_content == NULL)
-		text_content = "";
 
 	while (text_content[i] != '\0')
 	{
@@ -22,14 +20,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
-	if (fd == -1)
-		return(-1);
 
+	fd = open(filename, O_CREAT | O_RDWR | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 
-	write(fd, text_content, i);
+	w = write(fd, text_content, i);
+	if (w == -1)
+		return (-1);
 
 	close(fd);
 	return (1);
