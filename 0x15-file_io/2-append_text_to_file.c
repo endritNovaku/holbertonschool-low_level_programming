3b66d88219aa400bf43638661ab999f3fb3f7381
endritNovaku <ennald@outlook.com>
2022-03-29 00:41:05 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,9 +17,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i] != '\0')
+	if (text_content != NULL)
 	{
-		i++;
+		while (text_content[i] != '\0')
+			i++;
 	}
 
 	w = write(fd, text_content, i);
