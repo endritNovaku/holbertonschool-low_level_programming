b1a3b8d51a9de511652f04116e5648cfc6aaa89a
endritNovaku <ennald@outlook.com>
2022-03-29 00:37:32 -0700

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append text to file
+ * @filename: name of the file
+ * @text_content: text content
+ * Return: 1 on success else -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, w;
+	int i = 0;
+
+	fd = open(filename, O_RDWR | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	w = write(fd, text_content, i);
+	write(fd, '\0', i + 1);
+	if (w == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
