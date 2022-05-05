4d9b280a9a029defba6f7e5b5152d8699d62b167
endritNovaku <ennald@outlook.com>
2022-05-05 05:02:08 -0700

    add a node at a given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (idx > 1)
 	{
 		if (tmp == NULL)
-			return (NULL)
+			return (NULL);
 		tmp = tmp->next;
 		idx--;
 	}
