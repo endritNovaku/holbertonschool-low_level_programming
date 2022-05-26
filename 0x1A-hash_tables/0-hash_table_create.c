f93528200bf6198d451b10e4863dc48c9f2e2a30
endritNovaku <ennald@outlook.com>
2022-05-26 02:53:50 -0700

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_create - create a hash function
+ * @size: size of the has function
+ * Return 0
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash;
+	hash = malloc(sizeof(hash_table_t));
+	if (hash == NULL)
+		return (NULL);
+	hash->size = size;
+	hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash->array == NULL)
+		return (NULL);
+	return (hash);
+}
