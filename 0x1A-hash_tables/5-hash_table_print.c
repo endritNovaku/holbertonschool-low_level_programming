3ddba93c75338b5d77eeb9c7b4284a53acd081ff
endritNovaku <ennald@outlook.com>
2022-05-27 02:56:01 -0700

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,8 +6,22 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
-	unsigned long int i;
+	hash_node_t *node, *tmp;
+	unsigned long int i, j;
+	char *s;
+	
+	s = malloc(sizeof(char *) * 1024);
+
+	for (j = 0; j < ht->size; j++)
+        {
+		if (ht ->array != NULL)
+                        tmp = ht->array[j];
+                while (tmp != NULL)
+                {
+			s = tmp->key;
+                        tmp = tmp->next;
+                }
+        }
 
 	putchar('{');
 	for (i = 0; i < ht->size; i++)
@@ -16,7 +30,9 @@ void hash_table_print(const hash_table_t *ht)
 			node = ht->array[i];
 		while (node != NULL)
 		{
-			printf("'%s': '%s', ", node->key, node->value);
+			printf("'%s': '%s'", node->key, node->value);
+			if (node->key != s)
+				printf(", ");
 			node = node->next;
 		}
 	}
