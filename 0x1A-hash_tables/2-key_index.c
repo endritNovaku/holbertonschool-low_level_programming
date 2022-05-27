871993546bff8674a1a4231ba8b06dd0d1194ad4
endritNovaku <ennald@outlook.com>
2022-05-27 01:27:37 -0700

    fix

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
-
+/**
+ * key_index - key index
+ * @key: key
+ * @size: size of the array
+ * Return: hash_djb2(key) % size or 0
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (key == NULL || size == 0)
