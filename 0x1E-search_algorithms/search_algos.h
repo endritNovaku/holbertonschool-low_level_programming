21c2a9e01252989f99d68e309e368b78158c1e43
endritNovaku <ennald@outlook.com>
2022-08-30 03:32:00 -0700

    fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,5 +3,4 @@
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int *sorted_array(int *array, size_t size);
 #endif
